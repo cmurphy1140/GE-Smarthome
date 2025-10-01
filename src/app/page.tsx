42ef62e428cbbfa3db1ab5c3ddb79408ecd9827f
Connor Murphy <cmurphy1140@gmail.com>
2025-10-01 07:52:58 -0400

    ✨ Combine new Ronas IT-style sections with original homepage content - unified long-scroll page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { Footer } from '@/components/layout/Footer'
 import { HeroSection } from '@/components/sections/HeroSection'
 import { WhatWeDoSection } from '@/components/sections/WhatWeDoSection'
 import { SocialSidebar } from '@/components/common/SocialSidebar'
+import { PillarsSection } from '@/components/sections/PillarsSection'
+import { StatsSection } from '@/components/sections/StatsSection'
+import CyncFeaturesSection from '@/components/sections/CyncFeaturesSection'
 
 export default function Home() {
   return (
@@ -12,8 +15,16 @@ export default function Home() {
       <Header />
       <SocialSidebar />
       <main className="space-y-0">
+        {/* New Ronas IT-style sections */}
         <HeroSection />
         <WhatWeDoSection />
+        
+        {/* Original homepage sections */}
+        <section id="program">
+          <PillarsSection />
+        </section>
+        <StatsSection />
+        <CyncFeaturesSection />
       </main>
       <Footer />
     </div>
